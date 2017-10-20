f190762d979e78dfc4b9a938a43b58094741cfb5
Shane Laymance <slaymance@slaymance-xps.localdomain>
2017-10-20 16:45:46 -0700

    Set up server test file

---
+++ server/spec/serverSpec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import request from 'request';
+
+describe('Client Server', () => {
+  it('Should answer GET requests', done => {
+    request
+      .get('http://localhost:3000', (err, res, body) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+  });
+});
