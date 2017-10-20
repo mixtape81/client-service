22b4d0dbf4bcde5b8fd3684387d8ca3c6ab3145a
Shane Laymance <slaymance@slaymance-xps.localdomain>
2017-10-19 21:39:36 -0700

    Set up simple graphql endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 import express from 'express';
-const app = express();
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
 
+const app = express();
 const port = 3000;
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 'Hello world!' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
