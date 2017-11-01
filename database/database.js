34f901ab2d4da79c10880bb8abdfae3206f9937b
Shane Laymance <shane.laymance@gmail.com>
2017-10-31 20:01:03 -0700

    Refactor database connection for docker container

--- database/database.js
+++ database/database.js
@@ -3,13 +3,15 @@ import pgCreds from '../credentials/pgCreds';
 import LocationSchema from './models/Locations';
 import UserSchema from './models/Users';
 
-const databaseName = process.env.NODE_ENV === 'test' ?
-  pgCreds.databaseSpec : pgCreds.database;
+const databaseName = process.env.POSTGRES_DB || process.env.NODE_ENV === 'test'
+  ? pgCreds.databaseSpec : pgCreds.database;
+const databaseUsername = process.env.POSTGRES_USER || pgCreds.username;
+const databasePassword = process.env.POSTGRES_PASSWORD || pgCreds.password;
 
 const db = new Sequelize({
   database: databaseName,
-  username: pgCreds.username,
-  password: pgCreds.password,
+  username: databaseUsername,
+  password: databasePassword,
   dialect: 'postgres',
   logging: false
 });
